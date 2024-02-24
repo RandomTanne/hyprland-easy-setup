2dbf7d61dc3b4229e3a9adec34ef3e5291c7428d
RandomTanne <123188720+RandomTanne@users.noreply.github.com>
2024-02-24 17:35:45 +0100

    remove git from installation in script

--- hyprland-easy-setup.sh
+++ hyprland-easy-setup.sh
@@ -1,4 +1,4 @@
-pacman -Syu base-devel git 
+pacman -Syu base-devel 
 (cd /tmp/ && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si)
 yay -Syu nano terminator neofetch firefox hyprland nautilus hyprpaper waybar zsh
 cp hyprland.conf hyprpaper.conf ~/.config/hypr && mkdir ~/wallpapers
