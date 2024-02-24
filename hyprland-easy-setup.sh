93ba257baf1d5ae221c682ad05f739c7f8ee1da3
RandomTanne <123188720+RandomTanne@users.noreply.github.com>
2024-02-24 18:32:38 +0100

    add font to downloaded packages, that is necessary for waybar

--- hyprland-easy-setup.sh
+++ hyprland-easy-setup.sh
@@ -1,6 +1,6 @@
 pacman -Syu base-devel 
 (cd /tmp/ && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si)
-yay -Syu nano terminator neofetch firefox hyprland nautilus hyprpaper waybar zsh ulauncher
+yay -Syu nano terminator neofetch firefox hyprland nautilus hyprpaper waybar zsh otf-font-awesome ulauncher
 cp hyprland.conf hyprpaper.conf ~/.config/hypr && mkdir ~/wallpapers
 mkdir ~/.config/waybar && cp waybar/* ~/.config/waybar
 echo Installation finished
