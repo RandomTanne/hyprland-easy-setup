1267e399402aaad75376f13dbf7a62ae96818fba
RandomTanne <123188720+RandomTanne@users.noreply.github.com>
2024-02-24 18:35:19 +0100

    remove installation finished info and executing of zsh from script and instead reboot at the end of script installation

--- hyprland-easy-setup.sh
+++ hyprland-easy-setup.sh
@@ -3,5 +3,4 @@ pacman -Syu base-devel
 yay -Syu nano terminator neofetch firefox hyprland nautilus hyprpaper waybar zsh otf-font-awesome ulauncher
 cp hyprland.conf hyprpaper.conf ~/.config/hypr && mkdir ~/wallpapers
 mkdir ~/.config/waybar && cp waybar/* ~/.config/waybar
-echo Installation finished
-exec zsh
+exec reboot
