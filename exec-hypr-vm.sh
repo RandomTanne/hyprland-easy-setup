62db2f1ac96b2996b045d2b07f4e9750b8246f9e
RandomTanne <123188720+RandomTanne@users.noreply.github.com>
2024-02-24 17:57:57 +0100

    add script to execute hyprland on vms

---
+++ exec-hypr-vm.sh
@@ -0,0 +1,4 @@
+# This script is to execute hyprland on a virtual machine. Make sure to have 3D acceleration enabled in your VMs properties.
+export WLR_NO_HARDWARE_CURSORS=1
+export WLR_RENDERER_ALLOW_SOFTWARE=1
+exec Hyprland
