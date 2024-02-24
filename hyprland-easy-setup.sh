93d5d12da1d201b57f99898d988f0245836ae56f
RandomTanne <123188720+RandomTanne@users.noreply.github.com>
2024-02-24 17:09:12 +0100

    add zsh to installed packages and execute zsh at end of installation

--- hyprland-easy-setup.sh
+++ hyprland-easy-setup.sh
@@ -1,5 +1,6 @@
 pacman -Syu base-devel git 
 (cd /tmp/ && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si)
-yay -Syu nano terminator neofetch firefox hyprland nautilus hyprpaper waybar
+yay -Syu nano terminator neofetch firefox hyprland nautilus hyprpaper waybar zsh
 cp hyprland.conf hyprpaper.conf ~/.config/hypr && mkdir ~/wallpapers
 mkdir ~/.config/waybar && cp waybar/* ~/.config/waybar
+exec zsh
