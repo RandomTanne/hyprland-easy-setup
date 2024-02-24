d0ef8f538940e0777e50c4da8450327d19658670
RandomTanne <123188720+RandomTanne@users.noreply.github.com>
2024-02-24 16:48:11 +0100

    add hyprpaper config and setup script

--- hyprland-easy-setup.sh
+++ hyprland-easy-setup.sh
@@ -1,4 +1,5 @@
-pacman -Syu base-devel nano git terminator neofetch firefox hyprland nautilus hyprpaper waybar
+pacman -Syu base-devel git 
 (cd /tmp/ && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si)
+yay -Syu nano terminator neofetch firefox hyprland nautilus hyprpaper waybar
 cp hyprland.conf hyprpaper.conf ~/.config/hypr && mkdir ~/wallpapers
 mkdir ~/.config/waybar && cp waybar/* ~/.config/waybar
