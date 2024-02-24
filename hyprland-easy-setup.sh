5b5abc6a2134bf3d3d851c28560ba726218ecfb1
RandomTanne <123188720+RandomTanne@users.noreply.github.com>
2024-02-24 18:28:07 +0100

    add installation finshed message to installation script and add ulauncher to installed packages

--- hyprland-easy-setup.sh
+++ hyprland-easy-setup.sh
@@ -1,6 +1,7 @@
 pacman -Syu base-devel 
 (cd /tmp/ && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si)
-yay -Syu nano terminator neofetch firefox hyprland nautilus hyprpaper waybar zsh
+yay -Syu nano terminator neofetch firefox hyprland nautilus hyprpaper waybar zsh ulauncher
 cp hyprland.conf hyprpaper.conf ~/.config/hypr && mkdir ~/wallpapers
 mkdir ~/.config/waybar && cp waybar/* ~/.config/waybar
+echo Installation finished
 exec zsh
